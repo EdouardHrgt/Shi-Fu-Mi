3b6be6ec290df3dbccd16f2e19ec97f70750f486
Edouard Herrengt <herrengt.edouard@gmail.com>
2022-05-11 22:11:01 +0200

    Rework w/ vue js

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-// ########## RULES MODAL ########## //
-const rulesBtn = document.querySelector('.rules');
-const rulesModal = document.querySelector('.rules-modal');
-const closeModal = document.querySelector('.rules-modal svg');
-
-rulesBtn.addEventListener('click', () => {
-  rulesModal.style.display = 'flex';
-});
-
-closeModal.addEventListener('click', () => {
-  rulesModal.style.display = 'none';
+const vm = new Vue({
+  el: '#app',
+  data() {
+    return {
+      roles: [
+        {
+          name: 'paper',
+          image: './misc/svg/icon-paper.svg',
+          id: 'paper_outter',
+        },
+        {
+          name: 'scissors',
+          image: './misc/svg/icon-scissors.svg',
+          id: 'scissors_outter',
+        },
+        {
+          name: 'rock',
+          image: './misc/svg/icon-rock.svg',
+          id: 'rock_outter',
+        },
+      ],
+      userRole: undefined,
+      score: 0,
+      popup: false,
+      round: 1,
+    };
+  },
+  methods: {
+    togglePopup() {
+      this.popup == true ? (this.popup = false) : (this.popup = true);
+    },
+    backToChoice() {
+      this.round = 1;
+    },
+    pickedRole(role) {
+      this.userRole = role;
+      this.round = 2;
+    },
+  },
 });
