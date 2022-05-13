5e3d590e4daf63629ff8ec50399c88140a28aa3f
Edouard Herrengt <herrengt.edouard@gmail.com>
2022-05-13 14:40:11 +0200

    All JS logic OK

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const vm = new Vue({
       houseRole: undefined,
       score: 0,
       winner: '',
+      btnText: '',
       popup: false,
       round: 1,
       roles: [
@@ -43,21 +44,61 @@ const vm = new Vue({
     getHouseChoice(length) {
       return Math.floor(Math.random() * length);
     },
-    determineWinner(user, house) {
-      console.log(user.power, house.power);
-      /*
-      rock > scissors > paper > rock
-      3 > 2 > 1 > 3
-      */
-
-
+    scoreIncrement() {
+      if (this.winner == 'user') {
+        this.score += 1;
+        localStorage.setItem('score', JSON.stringify(this.score));
+      }
     },
-    pickedRole(role) {
+    btnTextDisplay() {
+      if (this.winner == 'user') {
+        this.btnText = 'YOU WIN';
+      } else if (this.winner == 'house') {
+        this.btnText = 'YOU LOSE';
+      } else if (this.winner == 'tie') {
+        this.btnText = 'IT\'S A TIE';
+      }
+    },
+    determineWinner(player, program) {
+      const user = player.power;
+      const house = program.power;
+      //TIE
+      if (user === house) {
+        this.winner = 'tie';
+        // NOT TIE
+      } else {
+        // USER = PAPER
+        if (user == 1 && house == 2) {
+          this.winner = 'house';
+        } else if (user == 1 && house == 3) {
+          this.winner = 'user';
+          // USER = SCISSORS
+        } else if (user == 2 && house == 1) {
+          this.winner = 'user';
+        } else if (user == 2 && house == 3) {
+          this.winner = 'house';
+          // USER = ROCK
+        } else if (user == 3 && house == 1) {
+          this.winner = 'house';
+        } else if (user == 3 && house == 2) {
+          this.winner = 'user';
+        }
+      }
+      // ADD POINTS TO THE SCORE
+      this.scoreIncrement();
+      // MODIFY BUTTON "play again" TEXT
+      this.btnTextDisplay();
+    },
+    battle(role) {
       this.userRole = role;
       this.round = 2;
       const rng = this.getHouseChoice(this.roles.length);
       this.houseRole = this.roles[rng];
       this.determineWinner(this.userRole, this.houseRole);
     },
+  },
+  created() {
+    if (!localStorage.getItem('score')) this.score = 0;
+    this.score = JSON.parse(localStorage.getItem('score'));
   },
 });
